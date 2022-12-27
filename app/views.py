43384e20fbfea03ff2099063da1a4b0f4c2aeebd
kirankumar73373 <kirankumar73373@gmail.com>
2022-12-27 16:38:48 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def meghana(request):
+    return render(request,'meghana.html')
+
+def kiran(request):
+    return render(request,'kiran.html')
